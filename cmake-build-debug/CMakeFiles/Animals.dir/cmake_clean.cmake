dd152db3ef38d4dd027a6ab72ee37d493fca6148
Daniel Shin <danielshin234@gmail.com>
2018-10-11 00:29:23 -0700

    init

---
+++ cmake-build-debug/CMakeFiles/Animals.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Animals.dir/main.cpp.o"
+  "Animals.pdb"
+  "Animals.exe"
+  "libAnimals.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Animals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
